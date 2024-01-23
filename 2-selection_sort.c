323145f2b8b864847e54809520d06e36b3dccff5
Yassine <hondayassin5@gmail.com>
2024-01-23 18:42:49 +0100

    commit of task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,28 +5,30 @@
  * @array: The array to be sorted
  * @size: The size of the array
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
-	int temp;
+    int temp;
 
-	if (!array || !size)
-		return;
-	for (i = 0; i < size - 1; i++)
+    if (!array || !size)
+    return;
+	for (i = 0; i < size; i++)
 	{
 		for (min = i, j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
-			{
-				min = j;
-			}
+            if (array[min] > array[j])
+            {
+                min = j;
+            }
 		}
-		if (array[i] > array[min])
-		{
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			print_array(array, size);
-		}
+        if (array[i] > array[min])
+        {
+        temp = array[min];
+        array[min] = array[i];
+        array[i] = temp;
+        print_array(array, size);
+        }
 	}
 }
+

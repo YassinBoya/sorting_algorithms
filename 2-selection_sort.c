581d4ef16d1c0defd421ccf1b9649e7ec7c0a464
Yassine <hondayassin5@gmail.com>
2024-01-23 18:33:21 +0100

    commit of task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,8 +16,7 @@ void selection_sort(int *array, size_t size)
     return;
 	for (i = 0; i < size; i++)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
+		for (min = i, j = i + 1; j < size; j++)
 		{
             if (array[min] > array[j])
             {
